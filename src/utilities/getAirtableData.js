830d7b3ab5d4e6f6ec82378e05a9662abc79717c
cdauria <cdauria@g.harvard.edu>
2023-09-18 16:37:25 -0400

    second monday update

--- src/utilities/getAirtableData.js
+++ src/utilities/getAirtableData.js
@@ -2,7 +2,7 @@
 import Airtable from "airtable";
 
 const getAirtableData = async (options) => {
-    const base = new Airtable({ apiKey: process.env.NEXT_PUBLIC_BASE_AIRTABLE_PAT }).base(process.env.NEXT_PUBLIC_PERSONALITY_BASE_ID);
+    const base = new Airtable({ apiKey: process.env.AIRTABLE_PAT }).base(process.env.BASE_ID);
     const records = await base('One').select({}).all();
     const processedData = records.map((record) => {
       console.log('Fields:', record.fields); // Log the fields of each record
